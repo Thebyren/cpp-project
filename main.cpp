d045d1c054200a88eeb769a5c9e013ff7b627d39
= <=>
2023-08-25 15:02:59 -0600

    se actualizo main y su ejecutable

--- main.cpp
+++ main.cpp
@@ -1,34 +1,32 @@
-#include <iostream>
-#include <curses.h>
+#include <ncurses.h>
 #include <string>
-#include <ctime>
-#include "jugador.h"
+#include <form.h>
 using namespace std;
-
+void winmain(){
+    init_pair(1,COLOR_BLUE,COLOR_BLACK);
+    attron(COLOR_PAIR(1));
+    box(stdscr,0,0);
+    attroff(COLOR_PAIR(1));
+}
 int main() {
-    initscr();
-    noecho();
-    cbreak();
+    initscr();  // Inicializa NCurses
+    cbreak();   // Deshabilita el búfer de línea (actuación inmediata de teclas)
+    echo();   // No muestres las teclas presionadas en la pantalla
+    keypad(stdscr, TRUE);  // Habilita el uso de teclas especiales
+    start_color(); // habilita el uso de colores por la terminal.
 
+    winmain();
+    int xMAX, yMAX;
 
-    //obtener datos de la pantalla
-    int yMAX, xMAX;
-    getmaxyx(stdscr, yMAX,xMAX);
+    getmaxyx(stdscr, yMAX, xMAX);
+    WINDOW * win1 = newwin(yMAX/2, xMAX/2, yMAX/4,xMAX/4);
+    box(win1, 0,0);
 
-    // crear la ventana
-    WINDOW * mainwin = newwin(20,50,(yMAX/2)-10,10);
-    box(mainwin,0,0);
+    mvwprintw(win1, 4,4,"eleccion");
+
     refresh();
-    wrefresh(mainwin);
-
-    Jugador * p = new Jugador(mainwin, 1,1 ,'@');
-    do{
-        p->display();
-        wrefresh(mainwin);
-    }while(p-> getmv()!='x');
-
-
-    //cerrar todo
-    endwin();
+    wrefresh(win1);
+    getch();
+    endwin();   // Finaliza NCurses
     return 0;
 }
