9a102aae3d6fc1bb434995a7343fdb5ede4f817a
= <=>
2023-08-28 14:08:29 -0600

    nada relevante, solo cambios minimos en variables

--- ventana.cpp
+++ ventana.cpp
@@ -3,9 +3,14 @@
 #include<cmath>
 using namespace std;
 
-string userInput(WINDOW *win, int x, int y, const std::string &message) {
+string userInput(WINDOW *win, int y, int x, const std::string &message) {
+    echo();
     wmove(win, y, x);
     wprintw(win, message.c_str());
+
+    // Dibuja la línea horizontal para simular el campo de entrada
+    mvwhline(win, y + 1, x + message.length(), ACS_S1, message.length());
+
     wrefresh(win);
 
     std::string input;
@@ -13,27 +18,40 @@ string userInput(WINDOW *win, int x, int y, const std::string &message) {
     int col = x + message.length();
 
     while ((ch = mvwgetch(win, y, col)) != KEY_F(2)) {
-    if (ch == '\n') {
-        break;
-    } else {
-        input += static_cast<char>(ch);
-        col++;
-        wrefresh(win);
+        if (ch == '\n') {
+            break;
+        } else if (ch == KEY_BACKSPACE || ch == 127) { // Tecla retroceso
+            if (!input.empty()) {
+                input.pop_back();
+                col--;
+                //mvwaddch(win, y, col, ' '); // Borra el caracter en la pantalla
+                mvwaddstr(win, y, col, "    ");
+                wrefresh(win);
+            }
+        } else {
+            input += static_cast<char>(ch);
+            col++;
+            wrefresh(win);
+        }
     }
-}
-
+    noecho();
     return input;
 }
 
-
 int main(){
     int yMAXIMO, xMAXIMO;
-    initscr();
     cbreak();
+    initscr();
+    noecho();
+    start_color();
+    init_pair(1, COLOR_BLACK, COLOR_MAGENTA);
+    attron(COLOR_PAIR(1));
+    box(stdscr,0,0);
+    attroff(COLOR_PAIR(1));
     getmaxyx(stdscr, yMAXIMO, xMAXIMO);
-
     refresh();
-    WINDOW * ventana = newwin(yMAXIMO,xMAXIMO,0,0);
+
+    WINDOW * ventana = newwin(yMAXIMO/2,xMAXIMO/2,yMAXIMO/4,xMAXIMO/4);
     box(ventana, 0,0);
     
     
@@ -45,9 +63,17 @@ int main(){
     float sueldoFLOAT = stof(sueldo);
     
     string resultado = to_string(round(sueldoFLOAT/2));
-    mvwprintw(ventana, 7,5, "la mitad de su sueldo es %s" , resultado.c_str());// muy importante el %s y c_str, 
+    mvwprintw(ventana, 8,6, "la mitad de su sueldo es %s" , resultado.c_str());// muy importante el %s y c_str, 
                                                                     //asi pueden agregar la variable
+    string nombre = userInput(ventana, 9,6, "ingrese su nombre: ");
+    mvwprintw(ventana, 11,6,"su nombre es: %s" , nombre.c_str());
+
     wrefresh(ventana);
     getch();
+    wclear(ventana);
+    box(ventana,0,0);
+    wrefresh(ventana);
+    getch();
+    
     return 0;
 }
