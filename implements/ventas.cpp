4485cf1ad44743f1a5e4b73b57ad7234ae008be7
= <=>
2023-08-31 03:21:37 -0600

    ya puedes ingresar las ventas

--- implements/ventas.cpp
+++ implements/ventas.cpp
@@ -1,5 +1,19 @@
-#include<iostream>
 #include"../headers/funciones.h"
+#include "../headers/managerDB.h"
+#include"../headers/menu.h"
+
 void funciones::ventas(const ParametrosVentana& params){
-    std::cout<<"hola desde ventas"<<std::endl;
+    SQLiteManager manager("base.db");
+    ventasUser venta;
+    wclear(params.ventana);
+    WINDOW*win = newwin(params.yMAX,params.xMAX,0,0);
+    box(win,0,0);
+    venta.idVenta = manager.obtenerIdVenta()+1;
+    venta.idVendedor = std::stoi(userInput(win, 2,3,"ingrese el id del vendedor: "));
+    venta.detalles= userInput(win, 4, 3, "ingrese los detalles de la compra:  ");
+    venta.monto = std::stoi(userInput(win,6,3,"ingrese el monto de la venta: "));
+    manager.querryDataBase(manager.newVenta(venta));
+    manager.closeDataBase();
+    wgetch(win);
+    wdeleteln(win);
 };
